61578a51f90f1a6f29a06ae4a64b9bcb693b5607
Srinu Meesala <srinusri76585@gmail.com>
2023-04-22 17:50:11 +0530

    first commit

--- src/assets/axiosSetup.js
+++ src/assets/axiosSetup.js
@@ -4,4 +4,5 @@ axios.defaults.headers.common["Accept"] = "application/json";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.put["Content-Type"] = "application/json";
 
-axios.defaults.baseURL = "http://localhost:4000";
+// axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://searchadsbe.onrender.com";
